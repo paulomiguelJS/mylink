d5d1b370a0c7638c691b7bd412c7e7f6042bd8b1
paulomigueljs <paulomiguelblues@gmail.com>
2022-02-04 15:48:40 -0300

    Home adjusted, starting reponsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import RoutesApp from './routes';
 
-import './App.css';
 
 function App() {
   return (
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,18 @@
 import {FiLink} from 'react-icons/fi'
+import './home.css'
 
 function Home() {
     return (
-      <div className="continer-home">
+      <div className="container-home">
         <div className="logo">
           <img src="./logo.svg" alt="" />
           <h1>My Link</h1>
           <span>Paste your link to shorten</span>
         </div>
+
         <div className="area-input">
           <div>
-              <FiLink size={24} color="#fff" />
+              <FiLink size={24} color="#fff" backgroundColor="transparent"  />
               <input placeholder='Paste your link here' />
           </div>
           <button>Generate Link</button>
