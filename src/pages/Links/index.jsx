0a4eb67336bb832c7d3e1539a17df53070b80152
paulomigueljs <paulomiguelblues@gmail.com>
2022-02-04 16:37:30 -0300

    links page styled, missing reponsive

--- src/pages/Links/index.jsx
+++ src/pages/Links/index.jsx
@@ -1,10 +1,29 @@
+import "./links.css";
+import { FiArrowLeft, FiLink, FiTrash } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
 function Links() {
-    return (
-      <div>
-       <h1>Links</h1>
+  return (
+    <div className="links-container">
+      <div className="links-header">
+        <Link to="/">
+          <FiArrowLeft size={38} color="#fff" />
+        </Link>
+        <h1>My Links</h1>
       </div>
-    );
-  }
-  
-  export default Links;
-  
+
+      <div className="links-item">
+        <button className="link">
+          <FiLink size={18} color="#fff" />
+          https://www.paulomiguel.dev/
+        </button>
+
+        <button className="link-delete">
+          <FiTrash size={24} color="#FF5454" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Links;
